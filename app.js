3a74f30015d0be11c93babf49aae7001df5ad61e
Bigslim <100359744+Bigslim@users.noreply.github.com>
2023-05-04 12:46:11 +0100

    2nd commit

---
+++ app.js
@@ -0,0 +1,49 @@
+const generateBtn = document.getElementById('generate-btn');
+const outputDiv = document.getElementById('output');
+
+generateBtn.addEventListener('click', () => {
+	const nameListTextarea = document.getElementById('name-list');
+	const numGroupsInput = document.getElementById('num-groups');
+	const nameList = nameListTextarea.value.trim().split('\n');
+	const numGroups = parseInt(numGroupsInput.value, 10);
+
+	if (!nameList || !numGroups || nameList.length < numGroups) {
+		alert('Please enter a valid list of names and number of groups.');
+		return;
+	}
+
+	const shuffledList = shuffleArray(nameList);
+	const groups = divideArrayIntoGroups(shuffledList, numGroups);
+	outputDiv.innerHTML = generateOutputHtml(groups);
+});
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}
+
+function divideArrayIntoGroups(array, numGroups) {
+	const groups = Array(numGroups).fill().map(() => []);
+	let currentGroupIndex = 0;
+	for (let i = 0; i < array.length; i++) {
+		groups[currentGroupIndex].push(array[i]);
+		currentGroupIndex = (currentGroupIndex + 1) % numGroups;
+	}
+	return groups;
+}
+
+function generateOutputHtml(groups) {
+	let html = '';
+	for (let i = 0; i < groups.length; i++) {
+		html += `<h2>Group ${i + 1}</h2>`;
+		html += '<ul>';
+		for (let j = 0; j < groups[i].length; j++) {
+			html += `<li>${groups[i][j]}</li>`;
+		}
+		html += '</ul>';
+	}
+	return html;
+}
---
+++ main.js
@@ -0,0 +1,37 @@
+// required modules
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// function to create a desktop window and initial window size
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1366,
+		height: 768,
+        // Node integration
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+	
+    // method to load the HTML file into the window
+    win.loadFile('index.html');
+}
+
+// method to wait the app to be ready and calling of the function to create the window
+app.whenReady().then(() => {
+	createWindow();
+    
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+// close the windows
+app.on('window-all-closed', () => {
+    // note that MacOS systems, "closing a window" don't terminate the program, to do so you have to use specific commands and paths
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
