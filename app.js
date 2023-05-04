af13289dff8f07cf8830ed29da616216a2b1c926
Bigslim <100359744+Bigslim@users.noreply.github.com>
2023-05-04 13:23:31 +0100

    3rd commit
    comments has been added

--- app.js
+++ app.js
@@ -7,30 +7,35 @@ generateBtn.addEventListener('click', () => {
     // makes the text area to be able to be fullfiled and interacts with the groups input
 	const nameListTextarea = document.getElementById('name-list');
 	const numGroupsInput = document.getElementById('num-groups');
-    // functions to clear up blank space and ignore blank lines 
+    // functions to clear up blank space and ignore blank lines on the form 
 	const nameList = nameListTextarea.value.trim().split('\n').filter(name => name.trim() !== '');
-    // and to parse values
+    // and to parse values on the selector
 	const numGroups = parseInt(numGroupsInput.value, 10);
     
-    //  
+    // for the present no code for opt in nor out specific rules for sorting       
 	if (!nameList || !numGroups || nameList.length < numGroups) {
+        // only a small alert if the number of groups is greater than its members
 		alert('Please enter a valid list of names and number of groups.');
 		return;
 	}
-
+    
+    // call the functions created below
 	const shuffledList = shuffleArray(nameList);
 	const groups = divideArrayIntoGroups(shuffledList, numGroups);
 	outputDiv.innerHTML = generateOutputHtml(groups);
 });
 
+// for shuffling the array
 function shuffleArray(array) {
 	for (let i = array.length - 1; i > 0; i--) {
+        // there is no such thing for "random" in a program language, this is a pseudorandom math formula understandable by a computer
 		const j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]];
 	}
 	return array;
 }
 
+// for sorting the elements in the groups
 function divideArrayIntoGroups(array, numGroups) {
 	const groups = Array(numGroups).fill().map(() => []);
 	let currentGroupIndex = 0;
@@ -41,6 +46,7 @@ function divideArrayIntoGroups(array, numGroups) {
 	return groups;
 }
 
+// for update the HTML with the scripted information
 function generateOutputHtml(groups) {
 	let html = '';
 	for (let i = 0; i < groups.length; i++) {
