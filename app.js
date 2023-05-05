77a3556250f85bf6e737d2e40c1c65b87930cbb2
Bigslim <100359744+Bigslim@users.noreply.github.com>
2023-05-05 10:25:06 +0100

    Revert "Update app.js"
    This reverts commit 739d70eeb7424babd657b2dda0d2e876a2567523.

--- app.js
+++ app.js
@@ -23,24 +23,17 @@ generateBtn.addEventListener('click', () => {
 	const shuffledList = shuffleArray(nameList);
 	const groups = divideArrayIntoGroups(shuffledList, numGroups);
 	outputDiv.innerHTML = generateOutputHtml(groups);
-	outputDiv.innerHTML = outputHtml;
 
-	// to write the output into a file A
-	const fs = require('fs');
-    fs.writeFile('groups.html', outputHtml, (err) => {
-        if (err) throw err;
-        console.log('File saved!');
-    });
-
-	// to write the output into a file opt B
-	/*
+	// to write the output into a file
 	const fileData = new Blob([outputHtml], {type: 'text/html'});
 	const downloadLink = document.createElement('a');
 	downloadLink.href = URL.createObjectURL(fileData);
 	downloadLink.download = 'groups.html';
 	downloadLink.click();
-	*/
 });
+
+// to display in the App
+outputDiv.innerHTML = outputHtml;
 
 // for shuffling the array
 function shuffleArray(array) {
