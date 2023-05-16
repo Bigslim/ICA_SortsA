becad3b3bdae3d91e1240e03d2d447d0fa63e263
Bigslim <100359744+Bigslim@users.noreply.github.com>
2023-05-16 16:52:52 +0100

    2nd commit
    IT is WORKING awesomely

--- app.js
+++ app.js
@@ -1,92 +1,104 @@
-'use strict'
-// read and interacts with the HTML elements refered
+/**
+ * this directive is used to enforce the declaration of keywords
+ * before the variables are used since JavaScript code can "infer"
+ * their use and implicitly declare them as global variables which
+ * may cause unexpected behavior and other bugs
+ */
+'use strict';
+
+/**
+ * the following constants have been declared to enable to get the 
+ * HTML references to be used in this code
+ */
 const generateBtn = document.getElementById('generate-btn');
 const outputDiv = document.getElementById('output');
-// a listener has to be added to save the generated groups. How? PDF, HTML, plain text?
 const saveBtn = document.getElementById('save-btn');
-// makes the button work as intended
+// declaring this variable outside the event listeners enable this version to run as intended
+let groups;
+
+// event listener for the Generate Teams button
 generateBtn.addEventListener('click', () => {
-	// makes the text area to be able to be fullfiled and interacts with the groups input
-	const nameListTextarea = document.getElementById('name-list');
-	const numGroupsInput = document.getElementById('num-groups');
-	// functions to clear up blank space and ignore blank lines on the form 
-	const nameList = nameListTextarea.value.trim().split('\n').filter(name => name.trim() !== '');
-	// and to parse values on the selector
-	const numGroups = parseInt(numGroupsInput.value, 10);
-	// for the present no code for opt in nor out specific rules for sorting       
-	if (!nameList || !numGroups || nameList.length < numGroups) {
-		// only a small alert if the number of groups is greater than its members
-		alert('Please enter a valid list of names and number of groups.');
-		return;
-	}
-	// call the functions created below
-	const shuffledList = shuffleArray(nameList);
-	const groups = divideArrayIntoGroups(shuffledList, numGroups);
-	// to display in the App
-	outputDiv.innerHTML = generateOutputHtml(groups);
+  // more HTML references
+  const nameListTextarea = document.getElementById('name-list');
+  const numGroupsInput = document.getElementById('num-groups');
+  // to remove empty lines and whitespaces
+  const nameList = nameListTextarea.value.trim().split('\n').filter(name => name.trim() !== '');
+  // parse and limit values on the selector
+  const numGroups = parseInt(numGroupsInput.value, 10);
+  // simple validation for this version
+  if (!nameList || !numGroups || nameList.length < numGroups) {
+    alert('Please enter a valid list of names and number of groups.');
+    return;
+  } // it can be heavly improved in the future versions
+
+  // variables and constants declared for the functions below
+  const shuffledList = shuffleArray(nameList);
+  groups = divideArrayIntoGroups(shuffledList, numGroups);
+
+  // displaying it in the program
+  outputDiv.innerHTML = generateOutputHtml(groups);
 });
 
+// event listener for the Save Teams button
 saveBtn.addEventListener('click', () => {
-	const generatedOutput = generateOutputText(groups);
-  
-	// Create a temporary <a> element
-	const downloadLink = document.createElement('a');
-	downloadLink.href = generateOutputUrl(generatedOutput);
-	downloadLink.download = 'teams';
-  
-	// Programmatically trigger the click event of the download link
-	downloadLink.click();
-  });
+  if (!groups) {
+    alert('Please generate teams first.');
+    return;
+  }
 
+  const generatedOutput = generateOutputText(groups);
 
-// for shuffling the array
+  const downloadLink = document.createElement('a');
+  downloadLink.href = generateOutputUrl(generatedOutput);
+  downloadLink.download = 'teams';
+  downloadLink.click();
+});
+
 function shuffleArray(array) {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
-	return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-// for sorting the elements in the groups
 function divideArrayIntoGroups(array, numGroups) {
-	const groups = Array(numGroups).fill().map(() => []);
-	let currentGroupIndex = 0;
-	for (let i = 0; i < array.length; i++) {
-		groups[currentGroupIndex].push(array[i]);
-		currentGroupIndex = (currentGroupIndex + 1) % numGroups;
-	}
-	return groups;
+  const groups = Array(numGroups).fill().map(() => []);
+  let currentGroupIndex = 0;
+  for (let i = 0; i < array.length; i++) {
+    groups[currentGroupIndex].push(array[i]);
+    currentGroupIndex = (currentGroupIndex + 1) % numGroups;
+  }
+  return groups;
 }
 
-// for update the HTML with the scripted information
 function generateOutputHtml(groups) {
-	let html = '';
-	for (let i = 0; i < groups.length; i++) {
-		html += `<h2>Group ${i + 1}</h2>`;
-		html += '<ul>';
-		for (let j = 0; j < groups[i].length; j++) {
-			html += `<li>${groups[i][j]}</li>`;
-		}
-		html += '</ul>';
-	}
-	return html;
+  let html = '';
+  for (let i = 0; i < groups.length; i++) {
+    html += `<h2>Group ${i + 1}</h2>`;
+    html += '<ul>';
+    for (let j = 0; j < groups[i].length; j++) {
+      html += `<li>${groups[i][j]}</li>`;
+    }
+    html += '</ul>';
+  }
+  return html;
 }
 
 function generateOutputText(groups) {
-	let text = '';
-	for (let i = 0; i < groups.length; i++) {
-	  text += `Group ${i + 1}:\n`;
-	  for (let j = 0; j < groups[i].length; j++) {
-		text += `${groups[i][j]}\n`;
-	  }
-	  text += '\n';
-	}
-	return text;
+  let text = '';
+  for (let i = 0; i < groups.length; i++) {
+    text += `Group ${i + 1}:\n`;
+    for (let j = 0; j < groups[i].length; j++) {
+      text += `${groups[i][j]}\n`;
+    }
+    text += '\n';
+  }
+  return text;
 }
 
 function generateOutputUrl(outputText) {
-	const encodedOutput = encodeURIComponent(outputText);
-	const dataUrl = `data:text/plain;charset=utf-8,${encodedOutput}`;
-	return dataUrl;
+  const encodedOutput = encodeURIComponent(outputText);
+  const dataUrl = `data:text/plain;charset=utf-8,${encodedOutput}`;
+  return dataUrl;
 }
